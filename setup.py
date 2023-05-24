a09b197d4e57dcab6694b11264d4e715ada550b9
GeertUU <121939243+GeertUU@users.noreply.github.com>
2023-05-24 11:28:47 +0200

    Update version number

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'surfaceanisotropycalculator',
-    version = '0.1.1',
+    version = '0.1.2',
 
     url='https://github.com/GeertUU/SurfaceAnisotropyCalculator',
     author='Geert',
