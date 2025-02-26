c6af14e458c5aef8f004bd084faebca46a33c112
GeertUU <121939243+GeertUU@users.noreply.github.com>
2025-02-26 16:13:10 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ cmdclass.update({ 'build_ext': build_ext })
 
 setup(
     name = 'surfaceanisotropycalculator',
-    version = '0.5.5',
+    version = '0.5.6',
 
     url='https://github.com/GeertUU/SurfaceAnisotropyCalculator',
     author='Geert',
