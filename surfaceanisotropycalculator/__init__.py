254a1eba5be48266470cabe3e356d107442c9053
Schulpen <g.h.a.schulpen@uu.nl>
2026-03-30 11:47:50 +0200

    Added removal of degenerate faces

--- surfaceanisotropycalculator/__init__.py
+++ surfaceanisotropycalculator/__init__.py
@@ -16,7 +16,7 @@
 '''
 
 
-__version__ = '0.5.10'
+__version__ = '0.5.11'
 
 from surfaceanisotropycalculator.anisotropyclass import MeshCalculator_legacy, MeshCalculator
 from surfaceanisotropycalculator.stackanalysis import MeshFromFile, MeshFromStack, MeshFromLif, MeshFromImageStack
